344a2177a7064575e1d60a37dac74053db407ae2
Muhammad HuzaiFa <muhammadhuzaifakhalil@hnhtechsolutions.com>
2024-09-26 01:24:09 +0500

    release1.2

--- src/screens/driverscreen/index.js
+++ src/screens/driverscreen/index.js
@@ -1,12 +1,22 @@
-import BudgetScreen from "./budgetscreen"
-import Drivinglicensescreen from "./driverregisterion"
-import experiencscreen from "./experinceescreen"
-import { Getstarted } from "./getstartedscreen"
-import { DRiverHome } from "./homescreen"
-import InappNavigationscreen from "./inappnavigation"
-import {DriverLoginScreen} from "./login"
-import UserOtpscreen from "./otpscreen"
-import Selectcityscreen from "./selectcity"
-import Vehicleinformaion from "./vechalinformation"
+import BudgetScreen from './budgetscreen';
+import Drivinglicensescreen from './driverregisterion';
+import experiencscreen from './experinceescreen';
+import {Getstarted} from './getstartedscreen';
+import {DRiverHome} from './homescreen';
+import InappNavigationscreen from './inappnavigation';
+import {DriverLoginScreen} from './login';
+import UserOtpscreen from './otpscreen';
+import Selectcityscreen from './selectcity';
+import Vehicleinformaion from './vechalinformation';
 
-export default {DRiverHome,DriverLoginScreen,Selectcityscreen,Drivinglicensescreen,Vehicleinformaion,InappNavigationscreen,Getstarted,UserOtpscreen,BudgetScreen}
+export default {
+  DRiverHome,
+  DriverLoginScreen,
+  Selectcityscreen,
+  Drivinglicensescreen,
+  Vehicleinformaion,
+  InappNavigationscreen,
+  Getstarted,
+  UserOtpscreen,
+  BudgetScreen,
+};
